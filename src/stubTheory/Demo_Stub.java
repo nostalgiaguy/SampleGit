c5b9bf5ab4ad54082e03e34a0b8c835c29d324c8
shubham pathak <pathakshubham28@gmail.com>
2015-02-19 23:17:33 +0530

    HW-commmit

--- src/stubTheory/Demo_Stub.java
+++ src/stubTheory/Demo_Stub.java
@@ -1,5 +1,26 @@
 package stubTheory;
 
-public class Demo_Stub {
+import org.junit.Assert;
+import org.junit.Test;
 
+
+
+class DayOrDark{
+	
+	public static String getDayDarkInfo(){
+		/*
+		 * Think and write how to find its day or dark.
+		 */
+		return "night";
+		
+	}
+}
+public class Demo_Stub {
+	
+	
+	@Test
+	public void testDayOrDark(){
+		Assert.assertEquals("night",DayOrDark.getDayDarkInfo());
+	}
+	
 }
