152ecc4582b0f9d9f8ed9d03b3d29129b1b287b4
Shubham Pathak <pathakshubham28@gmail.com>
2015-04-30 16:05:26 +0530

    30 april commit

--- SampleGit/src/com/nostalgiaguy/coreconceptpage4/Composition.java
+++ SampleGit/src/com/nostalgiaguy/coreconceptpage4/Composition.java
@@ -18,9 +18,25 @@ public class Composition {
 	private String s3, s4;
 	private int i;
 	private float toy;
-	private A obj;
+	private A1 obj;
 
+	public Composition(){
+		s3="to";
+		i=50;
+		toy=3.14F;
+		obj=new A1();
+	}
+	
+	public String toString(){
+		if(s4==null){
+			s4="you";
+		}
+		return "s1 = " + s1 + "\n" + "s2 = " + s2 + "\n" + "s3 = " + s3 + "\n"					 					 
+			 + "s4 = " + s4 + "\n" + "i = " + i + "\n" + "toy = " + toy + "\n" 
+		     + "castille = " + obj;					    
+	}
 	public static void main(String[] args) {
-		
+		Composition b=new Composition();
+		System.out.println(b);
 	}
 }
